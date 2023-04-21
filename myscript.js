97888a2ad9559dab366080674a0802ebef4a1706
AlessioAbbati <alex_117@live.it>
2023-04-21 13:55:19 +0200

    add js click

--- myscript.js
+++ myscript.js
@@ -5,3 +5,18 @@ const eleGrid = document.querySelector(".grid");
 for (let i = 0; i < 100; i++) {
     eleGrid.innerHTML += `<div class="cell"></div>`;
 }
+
+// utilizzo la classe clicked per colorare le caselle
+
+const listCells = document.querySelectorAll(".cell")
+
+for (let i = 0; i < listCells.length; i++) {
+    const cell = listCells[i];
+    cell.addEventListener("click",
+       function() {
+         this.classList.toggle("clicked");
+         console.log(this);
+       }
+    );
+
+}
