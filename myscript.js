899ddab64d39d8655ad82307fe696a27d88637e1
AlessioAbbati <alex_117@live.it>
2023-04-21 16:30:33 +0200

    mod css

--- myscript.js
+++ myscript.js
@@ -4,20 +4,26 @@ const eleGrid = document.querySelector(".grid");
 
 const eleBtnPlay = document.querySelector(".btn");
 
-for (let i = 0; i < 100; i++) {
-    eleGrid.innerHTML += `<div class="cell">${i + 1}</div>`;
-}
+eleBtnPlay.addEventListener("click",
+   function () {
+    for (let i = 0; i < 100; i++) {
+        eleGrid.innerHTML += `<div class="cell">${i + 1}</div>`;
+    }
+    
+    // utilizzo la classe clicked per colorare le caselle
+    const listCells = document.querySelectorAll(".cell")
+    
+    for (let i = 0; i < listCells.length; i++) {
+        const cell = listCells[i];
+        cell.addEventListener("click",
+           function() {
+             this.classList.toggle("clicked");
+             console.log(this);
+           }
+        );
+    
+    }
+    
+   }
+);
 
-// utilizzo la classe clicked per colorare le caselle
-const listCells = document.querySelectorAll(".cell")
-
-for (let i = 0; i < listCells.length; i++) {
-    const cell = listCells[i];
-    cell.addEventListener("click",
-       function() {
-         this.classList.toggle("clicked");
-         console.log(this);
-       }
-    );
-
-}
